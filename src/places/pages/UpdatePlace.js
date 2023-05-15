1b386c8a2e30ba91d1b1e484e2e2417611dba883
hyeonjin <gdalove@naver.com>
2023-05-16 00:32:09 +0900

    hooks 추가

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -5,11 +5,11 @@ import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import {
   VALIDATOR_REQUIRE,
-  VALIDATOR_MINLENGTH
+  VALIDATOR_MINLENGTH,
 } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import './PlaceForm.css';
-
+//장소명과 설명만을 업데이트
 const DUMMY_PLACES = [
   {
     id: 'p1',
@@ -20,9 +20,9 @@ const DUMMY_PLACES = [
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584
+      lng: -73.9878584,
     },
-    creator: 'u1'
+    creator: 'u1',
   },
   {
     id: 'p2',
@@ -33,68 +33,68 @@ const DUMMY_PLACES = [
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
-      lng: -73.9878584
+      lng: -73.9878584,
     },
-    creator: 'u2'
-  }
+    creator: 'u2',
+  },
 ];
 
 const UpdatePlace = () => {
   const placeId = useParams().placeId;
 
-  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId); //모든장소 검색 p.id가 placeId인 장소 찾음
 
   const [formState, inputHandler] = useForm(
     {
       title: {
         value: identifiedPlace.title,
-        isValid: true
+        isValid: true,
       },
       description: {
         value: identifiedPlace.description,
-        isValid: true
-      }
+        isValid: true,
+      },
     },
     true
   );
 
-  const placeUpdateSubmitHandler = event => {
+  const placeUpdateSubmitHandler = (event) => {
     event.preventDefault();
     console.log(formState.inputs);
   };
 
   if (!identifiedPlace) {
     return (
-      <div className="center">
+      <div className='center'>
         <h2>Could not find place!</h2>
       </div>
     );
   }
 
   return (
-    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+    <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
       <Input
-        id="title"
-        element="input"
-        type="text"
-        label="Title"
+        id='title'
+        element='input'
+        type='text'
+        label='Title'
         validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={inputHandler}
+        errorText='Please enter a valid title.'
+        onInput={inputHandler} //입력된 값과 해당값에 대한 유효성 저장
         initialValue={formState.inputs.title.value}
         initialValid={formState.inputs.title.isValid}
       />
       <Input
-        id="description"
-        element="textarea"
-        label="Description"
+        id='description'
+        element='textarea'
+        label='Description'
         validators={[VALIDATOR_MINLENGTH(5)]}
-        errorText="Please enter a valid description (min. 5 characters)."
+        errorText='Please enter a valid description (min. 5 characters).'
         onInput={inputHandler}
         initialValue={formState.inputs.description.value}
         initialValid={formState.inputs.description.isValid}
       />
-      <Button type="submit" disabled={!formState.isValid}>
+      <Button type='submit' disabled={!formState.isValid}>
         UPDATE PLACE
       </Button>
     </form>
